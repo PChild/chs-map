fa3caf63893685d135bc91f6d2aa21cf691e7ee3
Preston <childrep@vt.edu>
2019-07-15 02:04:00 -0400

    Renamed pop csvs, pulled city locations

--- pop_sizing.py
+++ pop_sizing.py
@@ -1,19 +1,69 @@
+from tqdm import tqdm
+import geocoder
 import pandas
+import csv
 
-'''Data from US Census Bureau City and Town Population Totals: 2010-2018
-https://www.census.gov/data/tables/time-series/demo/popest/2010s-total-cities-and-towns.html#ds
-'''
+region = 'chs'
 
-towns = [61, 71, 162, 170, 172]
-min_size = 24000 # Sized to include all previous DCMP locations (Fairfax is smallest at 24.5k pop)
-regions = ['virginia', 'maryland', 'dc']
 
-if __name__ == '__main__':
-    for region in regions:
-        region_df = pandas.read_csv(region + '.csv')
-        valid_locations = region_df.loc[region_df.SUMLEV.isin(towns)]
+def build_city_list():
+    towns = [61, 71, 162, 170, 172]
+    min_size = 24000  # Sized to include all previous DCMP locations (Fairfax is smallest at 24.5k pop)
+    if region is 'chs':
+        states = ['va', 'md', 'dc']
+    else:
+        states = ['ny']
+
+    '''Data from US Census Bureau City and Town Population Totals: 2010-2018
+    https://www.census.gov/data/tables/time-series/demo/popest/2010s-total-cities-and-towns.html#ds
+    '''
+    valid_cities = []
+    for state in states:
+        state_df = pandas.read_csv(state + '.csv')
+        valid_locations = state_df.loc[state_df.SUMLEV.isin(towns)]
         valid_locations = valid_locations.loc[valid_locations.POPESTIMATE2018 > min_size]
         valid_locations = valid_locations[['STNAME', 'NAME', 'POPESTIMATE2018']]
         valid_locations['NAME'] = valid_locations['NAME'].str[:-5]
-        print(region, len(valid_locations), 'valid locations:')
-        print(valid_locations)
+
+        for idx, row in valid_locations.iterrows():
+            valid_cities.append({'name': row.NAME, 'state': row.STNAME, 'pop': row.POPESTIMATE2018})
+
+    return valid_cities
+
+
+def save_city_list():
+    cities = sorted(build_city_list(), key=lambda k: k['pop'], reverse=True)
+    with open(region + '_city_names.csv', 'w', newline='') as outfile:
+        writer = csv.DictWriter(outfile, ['name', 'state', 'pop'])
+        writer.writeheader()
+
+        for city in cities:
+            writer.writerow(city)
+    outfile.close()
+
+
+def read_city_list():
+    with open(region + '_city_names.csv', 'r') as infile:
+        return [entry for entry in csv.DictReader(infile)]
+
+
+def locate_city(city):
+    lat, lng = geocoder.osm(city['name'] + ' ' + city['state']).latlng
+    return {'name': city['name'], 'lat': lat, 'lng': lng}
+
+
+def build_city_locations():
+    city_locations = []
+    for city in tqdm(read_city_list()):
+        city_locations.append(locate_city(city))
+
+    with open(region + '_city_locations.csv', 'w') as outfile:
+        writer = csv.DictWriter(outfile, ['name', 'lat', 'lng'])
+        writer.writeheader()
+
+        for city in city_locations:
+            writer.writerow(city)
+
+
+if __name__ == '__main__':
+    build_city_locations()
