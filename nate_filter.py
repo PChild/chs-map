e1321cd7a2829a88952e2f3ebbe290b206362354
Preston <childrep@vt.edu>
2019-07-22 23:25:03 -0400

    Creating active filter

--- nate_filter.py
+++ nate_filter.py
@@ -1,10 +1,19 @@
 import geopandas
+import tbapy
+import os
+
+tba = tbapy.TBA(os.getenv('TBA_KEY'))
+
+chs_teams = [int(team[3:]) for team in tba.district_teams('2019chs', keys=True)]
 
 test = geopandas.read_file('nate_chs.geojson')
 test['team'] = test['name'].str.split(' ').str[0]
 teams_df = test[test['team'].str.isnumeric()]
+teams_df['team'] = teams_df['team'].astype(int)
 teams_df['lat'] = teams_df['geometry'].y
 teams_df['lng'] = teams_df['geometry'].x
-teams_out = teams_df[['team', 'lat', 'lng']]
-teams_out.to_csv('nate_locs.csv', index=False)
+# fix this line, should compare to list of active teams, build column blah blah
+# teams_df['active'] =
+teams_out = teams_df[['team', 'lat', 'lng', 'active']]
+# teams_out.to_csv('nate_locs.csv', index=False)
 print(teams_out)
